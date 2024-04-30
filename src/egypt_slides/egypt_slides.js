cc5340075e22f0903c5d9d3139735a5be4748785
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-04-30 21:11:52 +0300

    deploying r

---
+++ src/egypt_slides/egypt_slides.js
@@ -0,0 +1,159 @@
+//react imports
+import React, { useState, useRef, createRef } from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+
+
+
+//imports of files 
+import './egypt_slides';
+import './egypt_slides_styling.css';
+
+//imports of media 
+import bg from '../images/theBg.png';
+import pyramids from '../images/pyramids.png';
+import sand from '../images/camlesCutOut.png';
+import camles from '../images/camles.png';
+//external elements
+import GuideMessageLayer from '../landing_guide_message/landing_message';
+
+function App() {
+
+// SEARCH ABOUT:: FILE FORMATTING IN RX NEED TO DIVIDE?
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const divRefs = useRef([createRef(), createRef(), createRef()]); // refrencefor each div
+  const [showOverlay, setShowOverlay] = useState(true);
+
+
+  const scrollToDiv = (index) => {
+    setActiveIndex(index);
+    divRefs.current[index].current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+
+
+  return (
+
+    <div className='theBigContainer'>
+      {showOverlay && (
+        <GuideMessageLayer onGotItClick={() => setShowOverlay(false)} />
+      )}
+
+      <div className="navigationDots">
+        {divRefs.current.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === activeIndex ? 'active' : ''}`}
+            onClick={() => scrollToDiv(index)}
+          >
+            {index === activeIndex ? <FontAwesomeIcon icon={faPlane} className="planeNavIcon" /> : ''}
+          </span>
+        ))}
+
+      </div>
+
+
+      {/* // pyramids Scene1 div */}
+      <div className="pyramidsScreenDiv" ref={divRefs.current[0]} id="div1">
+
+        <Parallax pages={2}>
+
+          {/* parallax bachground */}
+          <ParallaxLayer
+            speed={0.1}
+            factor={2}
+            style={{
+              backgroundImage: `url(${bg})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}>
+          </ParallaxLayer>
+
+          {/* parallax elements */}
+          <ParallaxLayer speed={0.3} offset={0}
+          >
+            <h1 className="sceneTitle">The pyramids</h1>
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={0.3} speed={0.15} className="parallax-layer-center">
+            <article className="infoArticle">
+              <p className="sceneParagraphInformation">
+                Enveloped in the golden hues of an ancient sunset, the Pyramids of Giza stand not merely as structures
+                but as timeless sentinels to history's magnificent tapestry. These colossal tombs, a testament to human ingenuity, transcend the label of a grave; they are a crossroads where tales of mythology, astronomy
+                , and mathematics converge.
+                With every stone etched they whisper stories of a civilization that viewed death not as an end but as a breathtaking beginning. Venture beyond the common narrative into the life of those who built them - a community
+                ; they cast light on the ingenuity of our ancestors, challenging visitors to look closer and perceive beyond the ordinary, into the realm of the extraordinary.    </p>
+            </article>
+          </ParallaxLayer>
+
+
+          <ParallaxLayer
+            factor={2}
+            offset={0.80}
+            speed={0.4}
+            style={{
+              backgroundImage: `url(${pyramids})`,
+              backgroundSize: '135% 60%',
+              backgroundPosition: 'center',
+            }}
+          >
+          </ParallaxLayer>
+
+
+
+          <ParallaxLayer className='parallaxSceneElement'
+            speed={0.1}
+            offset={0.99}
+            style={{
+              backgroundImage: `url(${sand})`,
+              backgroundSize: '100% 145%',
+              backgroundPosition: 'center',
+            }}>
+          </ParallaxLayer>
+
+          <ParallaxLayer className='parallaxSceneElement'
+            speed={0.03}
+            offset={0.99}
+            style={{
+              backgroundImage: `url(${camles})`,
+              backgroundSize: '100% 100%',
+              backgroundPosition: 'center',
+            }}>
+          </ParallaxLayer>
+        </Parallax>
+      </div>      {/* end of scene-- 1-- the pyramids */}
+
+
+      {/* ****************************scene 2 ***************************************/}
+      <div className="secondScreenDiv" ref={divRefs.current[1]} id="div2">
+        <Parallax
+          pages={2}
+        >
+
+          {/* parallax bachground */}
+          <ParallaxLayer
+            speed={0.1}
+            style={{
+              // backgroundImage: `url(${bg})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+
+
+      <div className="thirdScreenDiv" ref={divRefs.current[2]} id="div3">
+        <label>screen three lhad ma tigi isa</label>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
