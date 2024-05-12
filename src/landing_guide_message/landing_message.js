54ab26ae6801c96c5ce12589ff8cefdaddecab5b
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-05-13 00:21:14 +0300

    pop up 0.1

--- src/landing_guide_message/landing_message.js
+++ src/landing_guide_message/landing_message.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import './landing_message_styling.css'; 
+import './landing_message_styling.css';
 import clickingHand from '../images/click.gif';
 import dotsSS from '../images/Screenshot 2024-04-28 051325.png';
 import guideSS_GIF from '../images/guideSS_GIF.gif';
 import guidevideo from '../images/guideVideo.mp4';
 const GuideMessageLayer = ({ onGotItClick }) => {
   return (
-    <div className="guidanceLayer">
-      <div className='guidanceElements'>
-        <p className="guidanceParagraph">
-        definitely definitely definitely definitely STYLING WILL BE DONE TO ALL OF THAT...... Welcome to a small virtual wandering around Egypt. 
-            use the aeroplane on the navigation dots to go from a place to another with a click of a button. literally.
-            </p>
-            {/* <img src={guideSS_GIF} alt="Egypt Animated" className="clickGIF"/> */}
+    <div className="theAboveAll">
+      <div className="guidanceLayer">
+        <div className='guidanceElements'>
+          <p className="guidanceParagraph">
+            Ahlan wa Sahlan! Welcome to Your Journey Through Egypt! 🌍✨
 
-            <img src={clickingHand} alt="Egypt Animated" className="clickGIF"/>
-            <img src={dotsSS} alt="Egypt Animated" className="navigationDotsSS"/>
+            Embark on a captivating voyage across the timeless landscapes of Egypt by a click from your place. To wander through , please use the buttons labeled with the aeroplane ✈️ as the video below. 🠗
 
-          <video className="GuideVideo" controls>
-          <source src={guidevideo} type="video/mp4" />
-          issue with displaying guidance video
-        </video> 
+            Uncover the mysteries and marvels of Egypt—each button press !
 
-        <button className="gotItButton" onClick={onGotItClick}>Got it</button>
+          </p>
+          {/* <img src={guideSS_GIF} alt="Egypt Animated" className="clickGIF"/> */}
+
+        <div className='vidG'> 
+        <video className="GuideVideo" controls>
+            <source src={guidevideo} type="video/mp4" />
+            issue with displaying guidance video
+          </video><img src={clickingHand} alt="Egypt Animated" className="clickGIF"/>
+           {/*  <img src={dotsSS} alt="Egypt Animated" className="navigationDotsSS"/> */}
+
+          </div> 
+<div >
+          <button className="gotItButton" onClick={onGotItClick}>
+            Begin Your Adventure</button>
+        </div></div>
       </div>
     </div>
   );
